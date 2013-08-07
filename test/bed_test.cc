42fe3897c7493cc1368e256484586e03cb382300
David Z. Chen <d.zhekai.chen@gmail.com>
2013-08-07 13:26:31 -0700

    Committing latest.

--- test/bed_test.cc
+++ test/bed_test.cc
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include <bios/bed.hh>
 #include <gtest/gtest.h>
 
@@ -5,6 +7,13 @@ TEST(BedParser, DefaultConstructor) {
   bios::BedParser parser;
   bios::Bed* b = parser.NextEntry();
   EXPECT_EQ(NULL, b);
+}
+
+TEST(BedParser, ParseBasic) {
+  bios::BedParser parser;
+  parser.InitFromFile("./in/basic.bed");
+  bios::Bed* b = parser.NextEntry();
+  std::cout << b->ToString() << std::endl;
 }
 
 /* vim: set ai ts=2 sts=2 sw=2 et: */
